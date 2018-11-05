aa188338ff2d927da5f6b107fa00ebe2ed1c8353
kmalillos <malillosk@gmail.com>
2018-11-04 18:22:30 -0700

    working to use city buttons to search into weather API

--- assets/javascript/logic-weatherAPI.js
+++ assets/javascript/logic-weatherAPI.js
@@ -4,38 +4,42 @@
 // GLOBAL VARIABLES
 // =========================================================================================
 
-var city = "Denver";
+var city = "Denver"; //to dynamically fill with city buttons
 var APIkey = "89635bfae26331da7c18c8fa78db7fac";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=+" + city + "&appid=" + APIkey;
-
 
 // =========================================================================================
 // FUNCTIONS
 // =========================================================================================
 
-
-
+// function clickButton() {
+//   $(".city-button").on("click", function() {
+//       console.log("CLICK");
+//       console.log($(this).attr("city"));
+//   }); 
+// }
 
 // =========================================================================================
 // MAIN PROCESS
 // =========================================================================================
 
+// AJAX WORKS ***KEEP***
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
   
-  console.log(response);
+  //console.log(response);
  
   // city
-  console.log(response.name);
+  //console.log(response.name);
   var cityHolder = $("#city-holder");
       cityHolder.text("City: " + response.name)
 
   // temperature
-  console.log(response.main.temp);
+  //console.log(response.main.temp);
      var tempF = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(0);
-     console.log(tempF)
+     //console.log(tempF)
 
      var tempHolder = $("#temp-holder")
         tempHolder.text("Temp: " + tempF)
@@ -44,7 +48,7 @@ $.ajax({
   // weather description
   for (var i = 0; i < response.weather.length; i++) {
     
-    console.log(response.weather[i].description);
+    //console.log(response.weather[i].description);
 
     var weatherHolder = $("#weather-holder");
     var description = response.weather[i].description;
@@ -53,4 +57,4 @@ $.ajax({
 
   }
 
-});
+}); // close AJAX call
