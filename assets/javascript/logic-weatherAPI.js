f65b15e1c47f1451a62d92d7bdd021f87a8c93b4
kmalillos <malillosk@gmail.com>
2018-11-05 19:58:12 -0700

    fixed city button bugs - now integrates with Weather API

--- assets/javascript/logic-weatherAPI.js
+++ assets/javascript/logic-weatherAPI.js
@@ -48,23 +48,20 @@ $(document).on("click", ".city-button", function () {
     var cityHolder = $("#city-holder");
     cityHolder.text("City: " + response.name)
 
+
     // temperature
     //console.log(response.main.temp);
     var tempF = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(0);
     //console.log(tempF)
-
     var tempHolder = $("#temp-holder")
     tempHolder.text("Temp: " + tempF)
 
 
     // weather description
     //console.log(response.weather[i].description);
-
     var weatherHolder = $("#weather-holder");
     var description = response.weather[0].description;
-
     weatherHolder.text(description);
-
 
 
     //icon
@@ -101,58 +98,3 @@ $(document).on("click", ".city-button", function () {
   });
 
 });
-
-
-// =========================================================================================
-// C O D E  T H A T  W O R K S !
-// ========================================================================================
-
-
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function (response) {
-
-//   console.log(response);
-
-//   // city
-//   //console.log(response.name);
-//   var cityHolder = $("#city-holder");
-//   cityHolder.text("City: " + response.name)
-
-//   // temperature
-//   //console.log(response.main.temp);
-//   var tempF = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(0);
-//   //console.log(tempF)
-
-//   var tempHolder = $("#temp-holder")
-//   tempHolder.text("Temp: " + tempF)
-
-
-//   // weather description
-//   for (var i = 0; i < response.weather.length; i++) {
-
-//     //console.log(response.weather[i].description);
-
-//     var weatherHolder = $("#weather-holder");
-//     var description = response.weather[i].description;
-
-//     weatherHolder.append(description);
-
-//   //icon
-
-//   $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Denver&apikey=89635bfae26331da7c18c8fa78db7fac", function (data) {
-//     console.log(data);
-//     console.log(data.weather);
-//     console.log(data.weather[0]);
-//     console.log(data.weather[0].icon);
-
-//     var icon = data.weather[0].icon
-//     var url = 'http://openweathermap.org/img/w/' + icon + '.png'
-//     console.log(url);
-
-//     $(".icon").html("<img class = 'newIcon' src=" + url + ">");
-
-//   });
-
-// });
