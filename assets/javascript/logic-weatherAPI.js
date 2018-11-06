f98e9c59626fa015442b8ca5a3d69ef8c01c02d2
kmalillos <malillosk@gmail.com>
2018-11-05 20:06:35 -0700

    weather icon works with city button

--- assets/javascript/logic-weatherAPI.js
+++ assets/javascript/logic-weatherAPI.js
@@ -63,9 +63,9 @@ $(document).on("click", ".city-button", function () {
     var description = response.weather[0].description;
     weatherHolder.text(description);
 
-    
+
     //icon
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Denver&apikey=89635bfae26331da7c18c8fa78db7fac", function (data) {
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&apikey=89635bfae26331da7c18c8fa78db7fac", function (data) {
       console.log(data);
       console.log(data.weather);
       console.log(data.weather[0]);
