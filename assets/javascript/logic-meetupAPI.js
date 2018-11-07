6dfab71845231b1c2573e5ea14059878e35620e2
kmalillos <malillosk@gmail.com>
2018-11-06 22:02:51 -0700

    added google maps & meetup api integration and cleaned up code

--- assets/javascript/logic-meetupAPI.js
+++ assets/javascript/logic-meetupAPI.js
@@ -1,57 +1,117 @@
+// Google Maps APIkey: AIzaSyD_Ko_8jQz0fztSy-EG6klDU0Rw8rr5AT0 
 // Meetup API Key: 73526c5158647a3534730555e7b56b
-// NEED THIS: https://cors-anywhere.herokuapp.com/
-
-//google maps APIkey: AIzaSyD_Ko_8jQz0fztSy-EG6klDU0Rw8rr5AT0 
+// NEED THIS with Meetup: https://cors-anywhere.herokuapp.com/
 
 
-//Global variables
-var city = "";
-var APIkey = "73526c5158647a3534730555e7b56b";
-var CORSlink = "https://cors-anywhere.herokuapp.com/";
-var queryURL = CORSlink + "https://api.meetup.com/find/upcoming_events?key=" + APIkey + "&sign=true&photo-host=public&topic_category=hiking&page=20";
+// =========================================================================================
+// EVENT LISTENER FOR ON-CLICK OF CITY-BUTTON
+// =========================================================================================
 
-var meetUplink = "https://api.meetup.com/find/upcoming_events?key=73526c5158647a3534730555e7b56b&sign=true&photo-host=public&topic_category=hiking&page=20";
-var eventHolder;
+$(document).on("click", ".city-button", function () {
 
-//meetup api key
-$.ajax({
-	url: queryURL,
-  method: "GET"
-  
-}).then(function(response) {
-  console.log(response);
-  console.log(response.events);
+  // console.log($(this).attr("city"));
 
+  // =========================================================================================
+  // GOOGLE MAPS API AJAX CALL
+  // =========================================================================================
 
-  for (var i = 0; i < response.events.length; i++) {
-    $("#event-holder").append(response.events[i].name + " " + response.events[i].link);
+  var city = $(this).attr("city");
+  var googleAPIkey = "AIzaSyD_Ko_8jQz0fztSy-EG6klDU0Rw8rr5AT0";
+  var googleQueryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=" + googleAPIkey;
 
-    //console.log(response.events[i].name + " " + response.events[i].link);
-   
-    // $("#event-holder").append(response.events[i].name + " " + response.events[i].link);
-   
-    var eventHolder = $("#event-holder");
-    var list = $("<ul>").append("<li>" +response.events[i].name + " " + response.events[i].link + "</li>");
-
-    eventHolder.append(list);
-  }
-  
-  });
-
-  //creating two new variables
-  var lat; //adding the city.lat.lon from meetup link from this link https://api.meetup.com/find/upcoming_events?key=73526c5158647a3534730555e7b56b&sign=true&photo-host=public&topic_category=hiking&page=20
-  var lng;
-
-//google geocode api keys
   $.ajax({
-    url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=AIzaSyD_Ko_8jQz0fztSy-EG6klDU0Rw8rr5AT0",
+    url: googleQueryURL,
     method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    
-    // <--- this is the object of data you get back from the api... including the lat/long
-    // THEN use the lat/long to do your meetup api call in here!!!
-  });
+  }).then(function (response) {
+    //console.log(response);
+    console.log("City: " + response.results[0].address_components[0].short_name);
+    console.log("Lat: " + response.results[0].geometry.location.lat);
+    console.log("Long: " + response.results[0].geometry.location.lng);
+
+    var lat = response.results[0].geometry.location.lat;
+    var long = response.results[0].geometry.location.lng;
+
+    // =========================================================================================
+    // MEETUP API AJAX CALL
+    // =========================================================================================
+
+    var APIkey = "73526c5158647a3534730555e7b56b";
+    var CORSlink = "https://cors-anywhere.herokuapp.com/"
+    var queryURL = CORSlink + "https://api.meetup.com/find/upcoming_events?key=" + APIkey + "&sign=true&photo-host=public&lon=" + long + "&page=10&lat=" + lat;
+    // https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=-104.990251&page=10&lat=39.7392358
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      
+      // console.log(response);
+
+      var eventHolder = $("#event-holder");
+
+      eventHolder.empty();
+
+      for (var i = 0; i < response.events.length; i++) {
+
+        //console.log(response);
+        console.log(response.events[i]);
+
+        // $("#event-holder").append(response.events[i].name + " " + response.events[i].link);
+
+        var list = $("<ul>")
+        list.append("<li>" + response.events[i].name + "</li>");
+        
+        // var list = $("<ul>")
+        // list.append(
+        //   "<li>" + response.events[i].name + " " + response.events[i].link +
+        //   "</li>");
+
+        // var list = $("<ul>")
+        // list.append(
+        //   "<li>" + response.events[i].name +  " " + response.events[i].description + "</li>");
+
+        eventHolder.append(list);
+
+      }; // CLOSE FOR LOOP
+
+    }); // CLOSE MEETUP-API AJAX CALL
+
+  }); // CLOSE GOOGLE-MAPS-API AJAX CALL
+
+}); // CLOSE EVENT LISTENER FOR ON-CLICK FOR CLICK-BUTTON
 
 
 
+// =========================================================================================
+// C O D E  T H A T  W O R K S
+// =========================================================================================
+
+// MEET UP API AJAX CALL
+
+// var APIkey = "73526c5158647a3534730555e7b56b";
+// var CORSlink = "https://cors-anywhere.herokuapp.com/"
+// var queryURL = CORSlink + "https://api.meetup.com/find/upcoming_events?key=" + APIkey + "&sign=true&photo-host=public&page=10"
+
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// }).then(function (response) {
+//   console.log(response);
+
+//   for (var i = 0; i < response.events.length; i++) {
+
+//     console.log(response.events[i].name + " " + response.events[i].link);
+
+//     // $("#event-holder").append(response.events[i].name + " " + response.events[i].link);
+
+//     var eventHolder = $("#event-holder");
+//     var list = $("<ul>")
+//      list.append(
+//       "<li>" + response.events[i].name + " " + response.events[i].link + 
+//       "</li>");
+
+//       eventHolder.append(list);
+
+//     }  
+
+// });
